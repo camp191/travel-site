f9c37efe819af2b1725e21c4d2d294ac9bc264c8
Camp <thanapat423@gmail.com>
2016-09-07 23:27:08 +0700

    add gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+watch = require('gulp-watch'),
+postcss = require('gulp-postcss'),
+autoprefixer = require('autoprefixer'),
+cssvars = require('postcss-simple-vars'),
+cssnested = require('postcss-nested');
+
+gulp.task('default', function(){
+  console.log("Hello World from Gulp!");
+});
+
+gulp.task('html', function(){
+  console.log("Hello from HTML Hoo-Ray");
+})
+
+gulp.task('styles', function(){
+  return gulp.src('./app/assets/styles/styles.css')
+             .pipe(postcss([autoprefixer,cssvars,cssnested]))
+             .pipe(gulp.dest('./app/temp/styles'));
+
+})
+
+gulp.task('watch', function(){
+  watch('./app/index.html', function(){
+    gulp.start('html');
+  });
+
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('styles');
+  })
+})

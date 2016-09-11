d81989360f2f9a93443049475a5a1752ef349ba6
Thanapat Sorralump <CamPus@MacBook-Pro.local>
2016-09-12 00:56:55 +0700

    add postcss-mixin and test write

--- gulp/task/styles.js
+++ gulp/task/styles.js
@@ -3,11 +3,12 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 cssnested = require('postcss-nested'),
-cssimport = require('postcss-import');
+cssimport = require('postcss-import'),
+cssmixins = require('postcss-mixins');
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
-             .pipe(postcss([cssimport,autoprefixer,cssvars,cssnested]))
+             .pipe(postcss([cssimport, cssmixins, cssvars, cssnested, autoprefixer]))
              .on('error', function(errorInfo){
                console.log(errorInfo.toString());
                this.emit('end');

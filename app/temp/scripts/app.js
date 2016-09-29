15f4778de41ba564f956323fd0686fb2e67df4ec
camp191 <thanapat423@gmail.com>
2016-09-29 23:54:20 +0700

    Completed lazyloading

--- app/temp/scripts/app.js
+++ app/temp/scripts/app.js
@@ -11059,6 +11059,7 @@
 	  function StickyHeader() {
 	    _classCallCheck(this, StickyHeader);
 
+	    this.lazyImages = (0, _jquery2.default)(".lazyload");
 	    this.siteHeader = (0, _jquery2.default)(".site-header");
 	    this.headerTriggerElement = (0, _jquery2.default)(".large-hero__title");
 	    this.createHeaderWaypoint();
@@ -11066,9 +11067,17 @@
 	    this.headerLinks = (0, _jquery2.default)(".primary-nav a");
 	    this.createPageSectionWaypoints();
 	    this.addSmoothScroll();
+	    this.refreshWaypoints();
 	  }
 
 	  _createClass(StickyHeader, [{
+	    key: 'refreshWaypoints',
+	    value: function refreshWaypoints() {
+	      this.lazyImages.on("load", function () {
+	        Waypoint.refreshAll();
+	      });
+	    }
+	  }, {
 	    key: 'addSmoothScroll',
 	    value: function addSmoothScroll() {
 	      this.headerLinks.smoothScroll();
